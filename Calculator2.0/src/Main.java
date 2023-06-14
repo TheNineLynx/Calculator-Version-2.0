9bb51ba3238b6edd327055e02e746d9d2dad2a52
Sam Carpenter <135187486+TheNineLynx@users.noreply.github.com>
2023-06-14 17:45:59 +0800

    Delete Calculator2.0 directory

--- Calculator2.0/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        new MyFrame();
-    }
-}
