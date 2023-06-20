14fc45b60d939b176714e9d93a81c0ae144e9c71
Sam Carpenter <135187486+TheNineLynx@users.noreply.github.com>
2023-06-20 16:27:44 +0800

    Update MyFrame.java

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,8 +2,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
-    
+
+JPanel panel;
     MyFrame() {
+        panel = new JPanel();
         
         this.setSize(500, 900);
         this.setVisible(true);
@@ -11,9 +13,27 @@ public class MyFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setTitle("Calculator Version 2.0");
         this.setResizable(false);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 400, 400));
 
         ImageIcon logo = new ImageIcon("calculatorLogo.png");
         this.setIconImage(logo.getImage());
-        this.getContentPane().setBackground(Color.BLACK);
+        // this.getContentPane().setBackground(Color.BLACK);
+
+        panel.setPreferredSize(new Dimension (300, 300));
+        panel.setBackground(Color.black);
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+        this.add(panel);
+        this.setVisible(true);
     }
 }
